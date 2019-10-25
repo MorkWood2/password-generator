b437f9c286f16bd01ff3f2ef4d028a27baa9d2e1
Mark Wild <Mark.wild189@gmail.com>
2019-10-25 15:01:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-//DOM elements
-const resultEL = document.getElementById('result');
+//Dom elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboard = document.getElementById('clipboard');
 
 const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol
 }
+//generate event listen
+generateEL.addEventListener('click', () => {
+  const length = +lengthEl.value;
+	const hasLower = lowercaseEl.checked;
+	const hasUpper = uppercaseEl.checked;
+	const hasNumber = numbersEl.checked;
+	const hasSymbol = symbolsEl.checked;
 
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+      );
+});
+
+//Generate password function
+function generatePassword(lower, upper, number, symbol, length){
+  //1. init pw var
+  //2. filter out unchecked types
+  //3. loop over length call generator function for each types
+  //4. add final pw to the pw var and return
+
+    
+}
 //Generator functions - http://www.net-comber.com/charset.html
 
 function getRandomLower(){
