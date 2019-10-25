fa7474b31a5b676b240b0462e357fad622122545
Mark Wild <mark.wild189@gmail.com>
2019-10-25 12:12:43 -0700

    second commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//DOM elements
+const resultEL = document.getElementById('result');
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+}
+
+//Generator functions - http://www.net-comber.com/charset.html
+
+function getRandomLower(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getRandomSymbol(){
+  const symbols =  '!@#$%^&*(){}[]=<>/,.'
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
